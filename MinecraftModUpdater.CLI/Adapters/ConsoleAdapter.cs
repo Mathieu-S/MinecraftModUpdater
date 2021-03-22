a4a37e7d9e9c2afe3d9ab7b95573ea4a908db2c5
Mathieu-S <10650749+Mathieu-S@users.noreply.github.com>
2021-03-22 15:09:25 +0100

    ADD*: ConsoleAdapter for compatibility between CliFx & Spectre.Console

---
+++ MinecraftModUpdater.CLI/Adapters/ConsoleAdapter.cs
@@ -0,0 +1,18 @@
+﻿using CliFx;
+using Spectre.Console;
+
+namespace MinecraftModUpdater.CLI.Adapters
+{
+    internal static class ConsoleAdapter
+    {
+        public static IAnsiConsole ConvertToAnsiConsole(ref IConsole console)
+        {
+            return AnsiConsole.Create(new AnsiConsoleSettings
+            {
+                Ansi = AnsiSupport.Detect,
+                ColorSystem = ColorSystemSupport.Detect,
+                Out = console.Output
+            });
+        }
+    }
+}
