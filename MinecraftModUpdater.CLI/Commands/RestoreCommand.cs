99b89fbf3e7ac5fda199c01d0514ad5cc18edd26
Mathieu-S <10650749+Mathieu-S@users.noreply.github.com>
2021-03-22 15:12:22 +0100

    ADD*: RestoreCommand

---
+++ MinecraftModUpdater.CLI/Commands/RestoreCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using CliFx;
+using CliFx.Attributes;
+using MinecraftModUpdater.CLI.Adapters;
+using MinecraftModUpdater.Core.Services;
+using Spectre.Console;
+
+namespace MinecraftModUpdater.CLI.Commands
+{
+    [Command("restore", Description = "Restores the mods specified in a modlist.json.")]
+    public class RestoreCommand : ICommand
+    {
+        private readonly IModService _modService;
+        private readonly IModListFileService _modListFileService;
+
+        public RestoreCommand(IModService modService, IModListFileService modListFileService)
+        {
+            _modService = modService ?? throw new ArgumentNullException(nameof(modService));
+            _modListFileService = modListFileService ?? throw new ArgumentNullException(nameof(modListFileService));
+        }
+        
+        public async ValueTask ExecuteAsync(IConsole console)
+        {
+            var ansiConsole = ConsoleAdapter.ConvertToAnsiConsole(ref console);
+            var modListFile = await _modListFileService.ReadMinecraftModUpdaterFileAsync();
+            
+            if (modListFile.Mods.Count != 0)
+            {
+                foreach (var mod in modListFile.Mods)
+                {
+                    var modFile = await _modService.GetSpecificRelease(mod.Id, mod.Version);
+                    await _modService.DownloadModFileAsync(modFile);
+                }
+                
+                ansiConsole.Render(new Markup($"[lime]{modListFile.Mods.Count} mod(s) has been restored.[/]"));
+            }
+            else
+            {
+                ansiConsole.Render(new Markup("[yellow]There is no mods to install.[/]"));
+            }
+        }
+    }
+}
