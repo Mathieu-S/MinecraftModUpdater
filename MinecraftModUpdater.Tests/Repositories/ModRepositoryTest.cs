6973f8de13ad424684adebf82dca90a01e6cac46
Mathieu-S <10650749+Mathieu-S@users.noreply.github.com>
2021-04-19 15:35:34 +0200

    EDIT*: Enhance ModRepositoryTest

--- MinecraftModUpdater.Tests/Repositories/ModRepositoryTest.cs
+++ MinecraftModUpdater.Tests/Repositories/ModRepositoryTest.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading.Tasks;
+using MinecraftModUpdater.Core.Exceptions;
 using MinecraftModUpdater.Core.Models.Curse;
 using MinecraftModUpdater.Core.Repositories;
 using Xunit;
@@ -8,7 +10,7 @@ namespace MinecraftModUpdater.Tests.Repositories
     public class ModRepositoryTest
     {
         [Fact]
-        public async void GetModList()
+        public async void Get_Mod_List()
         {
             // Act
             var result = await ModRepository.GetModsAsync();
@@ -19,7 +21,7 @@ namespace MinecraftModUpdater.Tests.Repositories
         }
 
         [Fact]
-        public async void GetMod()
+        public async void Get_Mod()
         {
             // Act
             var result = await ModRepository.GetModAsync(238222);
@@ -30,7 +32,40 @@ namespace MinecraftModUpdater.Tests.Repositories
         }
 
         [Fact]
-        public async void GetModFiles()
+        public async Task Get_Mod_By_Invalid_Id()
+        {
+            // Act & Assert
+            await Assert.ThrowsAsync<CurseApiException>(() => ModRepository.GetModAsync(1));
+        }
+        
+        [Fact]
+        public async Task Get_Mod_By_Invalid_Number_Id()
+        {
+            // Act & Assert
+            await Assert.ThrowsAsync<ArgumentException>(() => ModRepository.GetModAsync(0));
+        }
+        
+        [Fact]
+        public async void Search_With_Name()
+        {
+            // Act
+            var result = await ModRepository.SearchModByNameAsync("JEI");
+
+            // Assert
+            Assert.NotNull(result);
+        }
+        
+        [Fact]
+        public async Task Search_Without_Name()
+        {
+            // Act & Assert
+            await Assert.ThrowsAsync<ArgumentNullException>(() => ModRepository.SearchModByNameAsync(null));
+        }
+        
+        
+        
+        [Fact]
+        public async void Get_ModFiles()
         {
             // Act
             var result = await ModRepository.GetModFilesAsync(238222);
@@ -38,9 +73,16 @@ namespace MinecraftModUpdater.Tests.Repositories
             // Assert
             Assert.NotNull(result);
         }
+        
+        [Fact]
+        public async void Get_ModFiles_Without_Id()
+        {
+            // Act & Assert
+            await Assert.ThrowsAsync<ArgumentException>(() => ModRepository.GetModFilesAsync(0));
+        }
 
         [Fact]
-        public async void DownloadModFile()
+        public async void Download_ModFile()
         {
             // Arrange
             var mod = new CurseModFile
@@ -55,6 +97,13 @@ namespace MinecraftModUpdater.Tests.Repositories
 
             // Assert
             Assert.NotNull(result);
+        }
+        
+        [Fact]
+        public async void Download_ModFile_Without_Arg()
+        {
+            // Act & Assert
+            await Assert.ThrowsAsync<ArgumentNullException>(() => ModRepository.GetStreamModFileAsync(null));
         }
     }
 }
